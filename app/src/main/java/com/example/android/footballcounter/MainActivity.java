d3877fc3aaffc98e23bf4f3d831ecfafae64a539
alez <alez1986@gmail.com>
2018-02-19 23:13:02 +0300

    Remove duplicate code

--- app/src/main/java/com/example/android/footballcounter/MainActivity.java
+++ app/src/main/java/com/example/android/footballcounter/MainActivity.java
@@ -8,13 +8,46 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+    private int scoreTeamA = 0;
+    private int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addThree(View v) {
+        String teamName = v.getTag().toString();
+        if (teamName.equals("teamA")) {
+            scoreTeamA += 3;
+            displayForTeamA(scoreTeamA);
+        } else {
+            scoreTeamB += 3;
+            displayForTeamB(scoreTeamB);
+        }
+    }
+
+    public void addTwo(View v) {
+        String teamName = v.getTag().toString();
+        if (teamName.equals("teamA")) {
+            scoreTeamA += 2;
+            displayForTeamA(scoreTeamA);
+        } else {
+            scoreTeamB += 2;
+            displayForTeamB(scoreTeamB);
+        }
+    }
+
+    public void addFive(View v) {
+        String teamName = v.getTag().toString();
+        if (teamName.equals("teamA")) {
+            scoreTeamA += 5;
+            displayForTeamA(scoreTeamA);
+        } else {
+            scoreTeamB += 5;
+            displayForTeamB(scoreTeamB);
+        }
     }
 
     // Team A
@@ -23,40 +56,10 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    public void addThreeTeamA(View v) {
-        scoreTeamA += 3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void addTwoTeamA(View v) {
-        scoreTeamA += 2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void addFiveTeamA(View v) {
-        scoreTeamA += 5;
-        displayForTeamA(scoreTeamA);
-    }
-
     // Team B
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
-    }
-
-    public void addThreeTeamB(View v) {
-        scoreTeamB += 3;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void addTwoTeamB(View v) {
-        scoreTeamB += 2;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void addFiveTeamB(View v) {
-        scoreTeamB += 5;
-        displayForTeamB(scoreTeamB);
     }
 
     // Reset both scores
